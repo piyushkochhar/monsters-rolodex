fc39e4a898f1f0c616558829f7fb6810af89f187
piyushkochhar <piyush.kochhar1@gmail.com>
2020-01-04 20:01:04 -0600

    Updates

---
+++ precache-manifest.f8252ba1a27be1a8b6ca4838f13ee7f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a4e4274fe729afeaffd0de26611286",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c2c21724c38b0a27316b",
+    "url": "/monsters-rolodex/static/css/main.986e1f37.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2c21724c38b0a27316b",
+    "url": "/monsters-rolodex/static/js/main.c45da75e.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
